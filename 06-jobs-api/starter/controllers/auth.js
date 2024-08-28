108366405ab77226be6a616103304deeccc36699
Abdulfattah Sheu <elsheikh.saf@gmail.com>
2024-08-28 14:51:42 +0100

    done

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,7 +1,22 @@
 const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
+const bcrypt = require('bcryptjs');
 
-const register = (req, res) => {
-  res.send(`register user`);
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const tempUser = { name, email, password: hashedPassword };
+
+  // if (!name || email || password) {
+  //   throw new BadRequestError('Please provide a name, email, and password');
+  // }
+
+  const user = await User.create({ ...tempUser });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 const login = (req, res) => {
   res.send(`login user`);
--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -22,7 +22,6 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide your password'],
     minlength: 6,
-    maxlength: 12,
   },
 });
 
